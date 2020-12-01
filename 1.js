5170486e6c2061a6aac797bd9d2e066cea2e5e1c
Wu Changdong <38553117+wuchangdong@users.noreply.github.com>
2020-12-01 21:16:43 +0800

    1. 两数之和

---
+++ 1.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (map[target-nums[i]] !== undefined) {
+            return [map[target-nums[i]], i];
+        }
+        map[nums[i]] = i;
+    }
+    return [0, 0];
+};
